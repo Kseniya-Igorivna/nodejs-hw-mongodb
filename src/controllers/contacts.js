6296815fbcb83f1c98fa41a99c85feeb91b5ccc3
Kseniya <159527696+Kseniya-Igorivna@users.noreply.github.com>
2024-11-25 22:11:11 +0200

    14\7

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,16 +47,21 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await updateContact(contactId, req.body);
-  if (!result) {
-    next(createHttpError(404, `Contact with id ${contactId} was not found`));
-    return;
+
+  try {
+    const updatedContact = await updateContact(contactId, req.body);
+    if (!updatedContact) {
+      return next(createHttpError(404, `Contact with id ${contactId} was not found`));
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully patched a contact!',
+      data: updatedContact,
+    });
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully patched a contact!',
-    data: result.data,
-  });
 };
 
 export const upsertContactController = async (req, res, next) => {
